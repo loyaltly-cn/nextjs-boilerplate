f8e65c1ca475a3d60575b38306e72b8f662b121a
loyal <loyaltly.cn@gmail.com>
2025-01-23 11:15:17 +0800

    update

--- app/server/api/send-email/route.js
+++ app/server/api/send-email/route.js
@@ -1,12 +1,21 @@
 import { NextResponse } from 'next/server';
 import { sendEmail } from '@/lib/smtp';
 
-export async function POST(request) {
-  const { to, subject, text, html } = await request.json();
+function generateCaptcha() {
+  let captcha = '';
+  for (let i = 0; i < 4; i++) {
+    captcha += Math.floor(Math.random() * 10); // 生成一个 0 到 9 之间的随机整数
+  }
+  return captcha;
+}
 
+export async function POST(request) {
+  const { to, subject, text, html } = await   request.json();
+  const code = generateCaptcha();
   try {
-    await sendEmail({ to, subject, text, html });
-    return NextResponse.json({ success: true, message: 'Email sent successfully' });
+    const captcha = generateCaptcha();
+    await sendEmail({ to, subject, text: captcha, html: captcha });
+    return NextResponse.json({ success: true, message: 'Email sent successfully', code });
   } catch (error) {
     return NextResponse.json({ success: false, message: 'Failed to send email', error });
   }
