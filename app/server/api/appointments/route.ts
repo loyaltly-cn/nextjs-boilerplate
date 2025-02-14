b6cf6e6cc46cecd934f4c0a108a21f8462a31e83
loyal <loyaltly.cn@gmail.com>
2025-02-15 03:42:12 +0800

    UPDATE

--- app/server/api/appointments/route.ts
+++ app/server/api/appointments/route.ts
@@ -4,7 +4,7 @@ import { prisma } from '@/lib/prisma'
 export async function POST(req: Request) {
   try {
     const body = await req.json()
-    const { userId, appointmentTime, type, name, phone, email, address, dateOfBirth, answers } = body
+    const { userId, appointmentTime, type, name, phone, email, address, dateOfBirth, answers ,beforeTime,zone} = body
 
     if (!userId || !appointmentTime) {
       return new NextResponse('Missing required fields', { status: 400 })
@@ -21,7 +21,9 @@ export async function POST(req: Request) {
         address,
         dateOfBirth:new Date(dateOfBirth),
         answers,
-        appointmentTime: new Date(appointmentTime)
+        appointmentTime: new Date(appointmentTime),
+        beforeTime: new Date(beforeTime),
+        zone
       }
     })
 
